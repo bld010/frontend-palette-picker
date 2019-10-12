00c7d10c669c92b60eb91ffe9c305a011d19b0ae
Brianna DelValle <Briannadelvalle@gmail.com>
2019-10-12 12:26:59 -0600

    App setup (#1)
    * Display random color palette on page load
    
    * Lock colors and display random colors on button click
    
    * Style color display divs
    
    * Add key to generated elements in colorDisplay

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import ColorDisplay from './ColorDisplay';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPalette: null
+    }
+  }
+
+  render = () => {
+
+    return(
+      <main className="App">
+        <h1>Palette Picker</h1>
+        <ColorDisplay palette={this.state.currentPalette} />
+      </main>
+    )
+  }
 }
 
 export default App;
---
+++ src/ColorDisplay.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+import randomColor from 'randomcolor';
+import './ColorDisplay.scss'
+import { MdLock, MdLockOpen } from 'react-icons/md'
+
+export default class ColorDisplay extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPalette: this.props.palette || null,
+      lockedColorIndices: []
+    }
+  }
+
+
+  componentDidMount = () => {
+    if (this.props.palette === null) {
+      this.generateNewRandomPalette();
+    }
+  }
+ 
+  toggleLock = (indexOfClickedColor) => {
+    let colors = this.state.currentPalette.colors;
+    let name = this.state.currentPalette.name;
+
+    let newColors = colors.map((color, index) => {
+      if (indexOfClickedColor === index) {
+        color.locked = !color.locked;
+        return color
+      } else { 
+        return color
+      }
+    })
+
+    this.setState({ currentPalette: {
+      name: name,
+      colors: newColors
+    }})
+  }
+
+  generateNewRandomPalette = () => {
+    let randomPalette = {
+      name: '',
+      colors: [
+        { hex: randomColor(), locked: false },
+        { hex: randomColor(), locked: false },
+        { hex: randomColor(), locked: false },
+        { hex: randomColor(), locked: false },
+        { hex: randomColor(), locked: false }
+      ]
+    }
+
+    this.setState({ currentPalette: randomPalette })
+  
+  }
+
+  getNewColors = () => {
+    let colors = this.state.currentPalette.colors;
+    let name = this.state.currentPalette.name;
+
+    let newColors = colors.map(color => {
+      if (color.locked) {
+        return color
+      } else {
+        return { hex: randomColor(), locked: false }
+      }
+    })
+
+
+
+    this.setState({ currentPalette: {
+      name: name,
+      colors: newColors
+    }})
+
+    this.generateColorsElements();
+  }
+
+  generateColorsElements = () => {
+    let colors = this.state.currentPalette.colors;
+
+    let colorsElements = colors.map((color, index) => {
+      return <div className="colorBlock" key={index}>
+        <div className="color"  style={{backgroundColor: color.hex}}>
+          <div className="lock" onClick={() => this.toggleLock(index)}>
+            {color.locked ? <MdLock size={62}/> : <MdLockOpen color="white" size={62}/>}
+            {color.locked ? <p>locked</p> : <></>}
+          </div>
+        </div>
+        </div>
+    })
+    
+    return colorsElements;
+  }
+
+  render = () => {
+    if (this.state.currentPalette !== null) {
+      let colorsElements = this.generateColorsElements()
+      return (
+        <div className="ColorDisplay">
+          {colorsElements}
+          <button onClick={this.getNewColors}>Click for more colors</button>
+        </div>
+      )
+    } else {
+      return <></>
+    }
+  }
+}
